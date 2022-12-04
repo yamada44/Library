96b0402547b1aacfda00b208a2f505bcd6b571c5
larenz jackson <romanlegion15@gmail.com>
2022-12-04 17:09:01 -0600

    test 2

--- Client_PresentMenuUI.lua
+++ Client_PresentMenuUI.lua
@@ -35,8 +35,8 @@ end
 
 function TargetPlayerClicked()
 	local playingplayers = filter(Game.Game.Players, function (p) return p.ID ~= Game.Us.ID end);
-	local options = map(playingplayers, "kol", PlayerButton);
-	UI.PromptFromList("Select the player you'd like to give armies to", options);
+	local options = map(playingplayers, PlayerButton);
+	UI.PromptFromList("Select the player you'd like to give armies to", options + "kol");
 end
 function PlayerButton(player)
 	local name = player.DisplayName(nil, false);
