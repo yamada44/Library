76660205e3bc0339b5e99ff8124c79fe5fd100d7
larenz jackson <romanlegion15@gmail.com>
2023-02-04 15:02:03 -0600

    Gift Gold Ultra update

--- Utilities2.lua
+++ Utilities2.lua
@@ -1,21 +1,4 @@
-function Dump(obj)
-	if obj.proxyType ~= nil then
-		DumpProxy(obj);
-	elseif type(obj) == 'table' then
-		DumpTable(obj);
-	else
-		print('Dump ' .. type(obj));
-	end
-end
-function DumpTable(tbl)
-    for k,v in pairs(tbl) do
-        print('k = ' .. tostring(k) .. ' (' .. type(k) .. ') ' .. ' v = ' .. tostring(v) .. ' (' .. type(v) .. ')');
-    end
-end
-function DumpProxy(obj)
-
-    print('type=' .. obj.proxyType .. ' readOnly=' .. tostring(obj.readonly) .. ' readableKeys=' .. table.concat(obj.readableKeys, ',') .. ' writableKeys=' .. table.concat(obj.writableKeys, ','));
-end
+--This file contains utility functions that could be used in any lua project.
 
 function split(str, pat)
    local t = {}  -- NOTE: use {n = 0} in Lua-5.0
