8887851419ca86da313bee5207a4ed550ce9964e
larenz jackson <romanlegion15@gmail.com>
2023-04-02 18:18:23 -0500

    Update Client_PresentSettingsUI.lua

--- Client_PresentSettingsUI.lua
+++ Client_PresentSettingsUI.lua
@@ -2,12 +2,14 @@
 function Client_PresentSettingsUI(rootParent)
 
 	local percent = Mod.Settings.Percent -- for games that are already running
+	local goldtax = Mod.Settings.GoldTax
+	if goldtax == nil then goldtax = 0 end
 	if percent == nil then percent = 0 end
 
-	if Mod.Settings.GoldTax > 0 then
+	if goldtax > 0 then
 		UI.CreateLabel(rootParent).SetText('Tax Mode: Tax multiplier').SetColor('#00F4FF')
 
-	UI.CreateLabel(rootParent).SetText('Players gifted gold are Taxed 1 peice of gold every time they exceed this number ( ' .. Mod.Settings.GoldTax .. ' ) ');
+	UI.CreateLabel(rootParent).SetText('Players gifted gold are Taxed 1 peice of gold every time they exceed this number ( ' .. goldtax .. ' ) ');
 	elseif percent > 0 then
 		UI.CreateLabel(rootParent).SetText('Tax Mode: Percent').SetColor('#00F4FF')
 		UI.CreateLabel(rootParent).SetText('Players gifted gold are Taxed ' .. percent .. '% for every transfer');
