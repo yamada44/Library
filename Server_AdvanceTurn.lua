b73dcfd5d43c658aa1c6ab3ac390071b4bca8afd
larenz jackson <romanlegion15@gmail.com>
2024-11-11 00:37:35 -0600

    Update Server_AdvanceTurn.lua

--- Server_AdvanceTurn.lua
+++ Server_AdvanceTurn.lua
@@ -26,37 +26,40 @@ function Server_AdvanceTurn_Order(game, order, result, skipThisOrder, addNewOrde
 				local ourid = publicgamedata.orderAlt[i].us
 				local MyID = publicgamedata.Entity[ourid].ID
 				local Trannumber = publicgamedata.orderAlt[i].Trannumber or 'Transaction'
-				if Trannumber ~= nil then
-					if MyID < 0 then MyID = 0 end
-
-					local localmessage = '(Local info) \n' .. goldsent  .. ' gold sent from ' .. publicgamedata.Entity[ourid].Name .. ' to ' .. publicgamedata.Entity[targetPlayerID].Name .. '\n#:' .. Trannumber
-					local publicmessage =  '(public info) \nUnknown amount of gold sent from ' .. publicgamedata.Entity[ourid].Name .. ' to ' .. publicgamedata.Entity[targetPlayerID].Name .. '\n#:' .. Trannumber
-					local revealmessage =  '(public info) \n' .. goldsent  .. ' gold sent from ' .. publicgamedata.Entity[ourid].Name .. ' to ' .. publicgamedata.Entity[targetPlayerID].Name .. '\n#:' .. Trannumber
-					local hiddenmessage =  '(public info) \n' .. goldsent .. ' gold sent from an unknown party to ' .. publicgamedata.Entity[targetPlayerID].Name .. '\n#:' .. Trannumber
+				local Ourname = publicgamedata.Entity[ourid].Name or "playerA"
+				local targetname = publicgamedata.Entity[targetPlayerID].Name or "PlayerB"
 
 
-					if(hiddenorder == true and publicgamedata.orderAlt[i].reveal == false)then
-						addNewOrder(WL.GameOrderEvent.Create(MyID, localmessage ,  {targetPlayerID}, nil, nil, {})); 
-						addNewOrder(WL.GameOrderEvent.Create(0, hiddenmessage , nil, nil, nil, {}));
+				if MyID < 0 then MyID = 0 end
+
+				local localmessage = '(Local info) \n' .. goldsent  .. ' gold sent from ' .. Ourname .. ' to ' .. targetname .. '\n#:' .. Trannumber
+				local publicmessage =  '(public info) \nUnknown amount of gold sent from ' .. Ourname .. ' to ' .. targetname .. '\n#:' .. Trannumber
+				local revealmessage =  '(public info) \n' .. goldsent  .. ' gold sent from ' .. Ourname .. ' to ' .. targetname .. '\n#:' .. Trannumber
+				local hiddenmessage =  '(public info) \n' .. goldsent .. ' gold sent from an unknown party to ' .. targetname .. '\n#:' .. Trannumber
 
 
-					elseif(hiddenorder == true or publicgamedata.orderAlt[i].reveal == true)then -- for players who want to reveal there gifted gold
+				if(hiddenorder == true and publicgamedata.orderAlt[i].reveal == false)then
+					addNewOrder(WL.GameOrderEvent.Create(MyID, localmessage ,  {targetPlayerID}, nil, nil, {})); 
+					addNewOrder(WL.GameOrderEvent.Create(0, hiddenmessage , nil, nil, nil, {}));
 
-						addNewOrder(WL.GameOrderEvent.Create(MyID, revealmessage ,  nil, nil, nil, {})); 
 
-					elseif(publicgamedata.orderAlt[i].reveal == false)then -- for players who dont
-					
-						-- creates message for players with visibility
-						addNewOrder(WL.GameOrderEvent.Create(MyID, localmessage ,  {targetPlayerID}, nil, nil, {})); 
-						addNewOrder(WL.GameOrderEvent.Create(MyID, publicmessage , nil, nil, nil, {}));
-						-- creates a message for everyone else who can't see the territory. handles no modifications 
-						-- this will create two messages for players who have visibility
-					end
+				elseif(hiddenorder == true or publicgamedata.orderAlt[i].reveal == true)then -- for players who want to reveal there gifted gold
+
+					addNewOrder(WL.GameOrderEvent.Create(MyID, revealmessage ,  nil, nil, nil, {})); 
+
+				elseif(publicgamedata.orderAlt[i].reveal == false)then -- for players who dont
+				
+					-- creates message for players with visibility
+					addNewOrder(WL.GameOrderEvent.Create(MyID, localmessage ,  {targetPlayerID}, nil, nil, {})); 
+					addNewOrder(WL.GameOrderEvent.Create(MyID, publicmessage , nil, nil, nil, {}));
+					-- creates a message for everyone else who can't see the territory. handles no modifications 
+					-- this will create two messages for players who have visibility
+				end
 				
 
 
 
-				end
+				
 
 				publicgamedata.orderaccess = false
 				publicgamedata.orderAlt = {}
