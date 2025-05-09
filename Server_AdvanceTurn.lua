bad6d4c879bf8d9e3dc06fab9930227d4b22ab58
larenz jackson <romanlegion15@gmail.com>
2025-05-09 10:14:41 -0500

    Update Server_AdvanceTurn.lua

--- Server_AdvanceTurn.lua
+++ Server_AdvanceTurn.lua
@@ -28,12 +28,9 @@ function Server_AdvanceTurn_Order(game, order, result, skipThisOrder, addNewOrde
 				local Trannumber = publicgamedata.orderAlt[i].Trannumber or 'Transaction'
 
 				if MyID < 0 then MyID = 0 end
-
-				local localmessage = '(Local info) \n' .. goldsent -- .. ' gold sent from ' ..  .. ' to ' .. .. '\n#:' .. Trannumber
-				--local localmessage =
-				local localmessage = publicgamedata.Entity[ourid].Name
-				local localmessage =  publicgamedata.Entity[targetPlayerID].Name
-				--local localmessage =
+print(publicgamedata.Entity[ourid].Name)
+print(publicgamedata.Entity[targetPlayerID].Name)
+				local localmessage = '(Local info) \n' .. goldsent  .. ' gold sent from ' .. publicgamedata.Entity[ourid].Name .. ' to ' .. publicgamedata.Entity[targetPlayerID].Name .. '\n#:' .. Trannumber
 				local publicmessage =  '(public info) \nUnknown amount of gold sent from ' .. publicgamedata.Entity[ourid].Name .. ' to ' .. publicgamedata.Entity[targetPlayerID].Name .. '\n#:' .. Trannumber
 				local revealmessage =  '(public info) \n' .. goldsent  .. ' gold sent from ' .. publicgamedata.Entity[ourid].Name .. ' to ' .. publicgamedata.Entity[targetPlayerID].Name .. '\n#:' .. Trannumber
 				local hiddenmessage =  '(public info) \n' .. goldsent .. ' gold sent from an unknown party to ' .. publicgamedata.Entity[targetPlayerID].Name .. '\n#:' .. Trannumber
